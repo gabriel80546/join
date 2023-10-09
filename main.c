3c5eec29d745fb3d2b7e33ea7a5218157a6d7575
Gabriel Passos <gabriel80546@gmail.com>
2023-10-09 13:14:38 -0300

    ./push.sh

--- main.c
+++ main.c
@@ -2,14 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+char *join(char *left, char *right) {
+    int i;
+    int tamanho;
+    char *saida;
+
+    tamanho = strlen(left) + strlen(right) + 1;
+    printf("join1: \"%s\" + \"%s\" = %i\n", left, right, (tamanho - 1));
+
+    saida = (char*) malloc(sizeof(char) * tamanho);
+    for(i = 0; i < (tamanho - 1); i++) {
+        saida[i] = 'X';
+    }
+    saida[tamanho] = '\0';
+    return saida;
+}
+
 int main() {
 
-    char str[80];
-    strcpy(str, "these ");
-    strcat(str, "strings ");
-    strcat(str, "are ");
-    strcat(str, "concatenated.");
-    printf("str = \"%s\"\n", str);
+    char str1[80];
+    strcpy(str1, "usando ");
+    strcat(str1, "strcat ");
+    strcat(str1, "para ");
+    strcat(str1, "juntar.");
+    printf("str = \"%s\"\n", str1);
+    printf("join() = %p\n", join);
+
+    printf("join() => \"%s\"\n", join("Ola", "Mundo"));
 
     return 0;
 }
