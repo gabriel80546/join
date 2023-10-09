97f68adaab3c887b1bd47ddd7b77761d3b3d8b63
Gabriel Passos <gabriel80546@gmail.com>
2023-10-09 18:41:47 -0300

    ./push.sh

--- main.c
+++ main.c
@@ -11,9 +11,14 @@ char *join(char *left, char *right) {
     printf("join1: \"%s\" + \"%s\" = %i\n", left, right, (tamanho - 1));
 
     saida = (char*) malloc(sizeof(char) * tamanho);
-    for(i = 0; i < (tamanho - 1); i++) {
-        saida[i] = 'X';
-    }
+
+
+
+
+
+    // for(i = 0; i < (tamanho - 1); i++) {
+    //     saida[i] = 'X';
+    // }
     saida[tamanho] = '\0';
     return saida;
 }
@@ -21,6 +26,7 @@ char *join(char *left, char *right) {
 int main() {
 
     char str1[80];
+    char *str2;
     strcpy(str1, "usando ");
     strcat(str1, "strcat ");
     strcat(str1, "para ");
@@ -28,7 +34,10 @@ int main() {
     printf("str = \"%s\"\n", str1);
     printf("join() = %p\n", join);
 
-    printf("join() => \"%s\"\n", join("Ola", "Mundo"));
 
+    str2 = join("Ola", "Mundo");
+    printf("join() => \"%s\"\n", str2);
+
+    free(str2);
     return 0;
 }
