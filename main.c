495acbb09bb558d54a724f0275570278ce767131
Gabriel Passos <gabriel80546@gmail.com>
2023-10-09 23:05:44 -0300

    ./push.sh

--- main.c
+++ main.c
@@ -3,37 +3,33 @@
 #include <string.h>
 
 char *join(char *left, char *right) {
-    int i;
+    int i, j;
     int tamanho;
     char *saida;
 
     tamanho = strlen(left) + strlen(right) + 1;
-    printf("join1: \"%s\" + \"%s\" = %i\n", left, right, (tamanho - 1));
-
     saida = (char*) malloc(sizeof(char) * tamanho);
 
-
-
-
-
-    // for(i = 0; i < (tamanho - 1); i++) {
-    //     saida[i] = 'X';
-    // }
+    for(i = 0; i < strlen(left); i++) {
+        saida[i] = left[i];
+    }
+    j = i;
+    for(i = 0; i < strlen(right); i++) {
+        saida[i + j] = right[i];
+    }
     saida[tamanho] = '\0';
     return saida;
 }
 
 int main() {
-
     char str1[80];
     char *str2;
+
     strcpy(str1, "usando ");
     strcat(str1, "strcat ");
     strcat(str1, "para ");
     strcat(str1, "juntar.");
     printf("str = \"%s\"\n", str1);
-    printf("join() = %p\n", join);
-
 
     str2 = join("Ola", "Mundo");
     printf("join() => \"%s\"\n", str2);
